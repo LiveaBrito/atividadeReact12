1bfc6f78317fb1cb038433b00f63fc7e85e2c811
Livea Brito <159591382+LiveaBrito@users.noreply.github.com>
2024-10-20 02:29:04 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Paragrafo from './components/Paragrafo'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Paragrafo/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Paragrafo.jsx
@@ -0,0 +1,8 @@
+function Paragrafo(){
+
+    return <p>A programação não é apenas sobre o que você faz, mas sobre como você pensa!</p>
+
+
+}
+
+export default Paragrafo
